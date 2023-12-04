8b34f0aee206092fa7d8322b914640a2108fb346
setiwon <45044735+setiwon@users.noreply.github.com>
2023-12-04 00:32:15 -0800

    password length check

--- src/pages/createAcc.js
+++ src/pages/createAcc.js
@@ -1,14 +1,14 @@
+import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import React, { useState } from "react";
 import { auth, db as firestore } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
 
-import Authentication from '../Authenticate';
-import nameIMG from '../images/person.png';
-import emailIMG from '../images/email.png';
-import passwordIMG from '../images/password.png';
-import '../style/createAcc.css';
+import Authentication from "../Authenticate";
+import nameIMG from "../images/person.png";
+import emailIMG from "../images/email.png";
+import passwordIMG from "../images/password.png";
+import "../style/createAcc.css";
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
@@ -18,10 +18,16 @@ const SignUp = () => {
 
   const signUp = async (e) => {
     e.preventDefault();
-    
+
+    if (password.length < 6) {
+      // Password is too short, show an alert
+      alert("Password must be 6 characters or longer.");
+      return;
+    }
+
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      
+
       // Save additional user data to Firestore, including the username
       await setDoc(doc(firestore, "users", userCredential.user.uid), {
         username,
@@ -37,14 +43,14 @@ const SignUp = () => {
 
   return (
     <form className="container" onSubmit={signUp}>
-      <div className="header"> 
+      <div className="header">
         <div className="text">Sign Up</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
         <div className="input">
           <img src={nameIMG} alt="username"></img>
-          <input 
+          <input
             type="text"
             placeholder="username"
             value={username}
@@ -53,7 +59,7 @@ const SignUp = () => {
         </div>
         <div className="input">
           <img src={emailIMG} alt="email"></img>
-          <input 
+          <input
             type="email"
             placeholder="email"
             value={email}
@@ -62,7 +68,7 @@ const SignUp = () => {
         </div>
         <div className="input">
           <img src={passwordIMG} alt="password"></img>
-          <input 
+          <input
             type="password"
             placeholder="password"
             value={password}
@@ -73,8 +79,12 @@ const SignUp = () => {
       <div>
       </div>
       <div className="submit-container">
-        <button className="submit" type="submit">Sign Up</button>
-        <Link to="/login" className="login">Login</Link>
+        <button className="submit" type="submit">
+          Sign Up
+        </button>
+        <Link to="/login" className="login">
+          Login
+        </Link>
       </div>
       <Authentication />
     </form>
