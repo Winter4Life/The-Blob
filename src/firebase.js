894bb429ed052668944600c058e0bae557fff756
unknown <taylork1201@yahoo.com>
2023-12-08 00:38:40 -0800

    styling

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from 'firebase/storage';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyDYg2cRdtMKjUQRfnUhUjgpRT7GAxRu6Lw",
-  authDomain: "interactive-blog-69659.firebaseapp.com",
-  projectId: "interactive-blog-69659",
-  storageBucket: "interactive-blog-69659.appspot.com",
-  messagingSenderId: "520979559260",
-  appId: "1:520979559260:web:0be7772b83f7462858c799",
-  measurementId: "G-PVDE8ZTCV0"
-}; */
 
 const firebaseConfig = {
   apiKey: "AIzaSyD3A4OUi0XCp0UV22YmAgDgdHJho6Zu8zo",
@@ -32,7 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, storage, provider, ref, uploadBytes, getDownloadURL };
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,8 +1,11 @@
-// Profile.js
 import React, { useEffect, useState } from "react";
-import { doc, getDoc,updateDoc, collection, query, where, getDocs } from "firebase/firestore";
-import { db, auth } from "../firebase";
+import { doc, getDoc, updateDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { db, auth, storage } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { uploadBytes, getDownloadURL } from "firebase/storage";
+
+import profileIMG from '../images/profile.png'
+import '../style/profile.css'
 
 function Profile() {
   const [userProfile, setUserProfile] = useState(null);
@@ -11,22 +14,24 @@ function Profile() {
   const [totalPosts, setTotalPosts] = useState(0);
   const [editingBio, setEditingBio] = useState(false);
   const [newBio, setNewBio] = useState("");
-   const navigate = useNavigate();
+  const [profileImage, setProfileImage] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const navigate = useNavigate();
 
-   useEffect(() => {
+  useEffect(() => {
     const fetchUserProfile = async () => {
       const user = auth.currentUser;
-  
+
       if (!user) {
         // Redirect to /createAcc if user is not logged in
         navigate("/createAcc");
         return;
       }
-  
+
       // Continue fetching user profile if the user is logged in
       const userDocRef = doc(db, "users", user.uid);
       const userDoc = await getDoc(userDocRef);
-  
+
       if (userDoc.exists()) {
         setUserProfile(userDoc.data());
         setNewBio(userDoc.data().bio || "");
@@ -59,65 +64,117 @@ function Profile() {
 
   const handleEditBio = async () => {
     const user = auth.currentUser;
-  
+
     if (!user) {
       console.error("User not authenticated");
       return;
     }
-  
+
     try {
       // Update user bio in Firestore
       const userDocRef = doc(db, "users", user.uid);
       await updateDoc(userDocRef, { bio: newBio });
-  
+
       console.log("Bio updated successfully!");
-  
+
       // Update state
       setUserProfile((prevProfile) => ({ ...prevProfile, bio: newBio }));
     } catch (error) {
       console.error("Error updating bio:", error);
     }
-  
+
     // Reset editing mode
     setEditingBio(false);
   };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setProfileImage(file);
+  };
+
+  const handleUploadImage = async () => {
+    try {
+      const user = auth.currentUser;
   
+      if (!user) {
+        console.error("User not authenticated");
+        return;
+      }
+  
+      if (profileImage) {
+        // Upload the image to storage
+        const storageRef = storage.ref();
+        const imageRef = storageRef.child(`profile-images/${user.uid}`);
+        await uploadBytes(imageRef, profileImage);
+  
+        // Get the image URL
+        const imageUrl = await getDownloadURL(imageRef);
+  
+        // Update the user's profile image in Firestore
+        const userDocRef = doc(db, "users", user.uid);
+        await updateDoc(userDocRef, { profileImage: imageUrl });
+  
+        // Update state
+        setUserProfile((prevProfile) => ({ ...prevProfile, profileImage: imageUrl }));
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  
+    // Reset edit mode
+    setEditMode(false);
+  };
+  
+
   if (!userProfile) {
     return <div>Loading...</div>;
   }
 
-  
-
   return (
-    <div className="profile">
-      <h1>{userProfile.username}'s Profile</h1>
-
-      {/* Bio Section */}
-      <div>
-        {editingBio ? (
-          <div>
-            <p>Edit Bio:</p>
-            <textarea value={newBio} onChange={(e) => setNewBio(e.target.value)} />
-            <button onClick={handleEditBio}>Save</button>
+    <div className="mainprofile-container">
+      <div className="profile-container">
+        <div className="profile-img">
+          <img
+            src={userProfile.profileImage || profileIMG}
+            alt="Profile"
+            style={{ maxWidth: "100px", borderRadius: "50%" }}
+          />
+          {editMode && (
+            <div>
+              <input type="file" onChange={handleImageChange} />
+              <button onClick={handleUploadImage}>Upload Image</button>
+            </div>
+          )}
+        </div>
+        <div className="profile-content">
+          <div className="profile-info">
+            <p className="username">@{userProfile.username}</p>
+            <p>Total Posts: ({totalPosts})</p>
+            <p>Total Likes Received: {likesReceived}</p>
           </div>
-        ) : (
-          <div>
-            <p>Bio: {userProfile.bio || "Not provided"}</p>
-            <button onClick={() => setEditingBio(true)}>Edit Bio</button>
+          <div className="profile-bio">
+            {editingBio ? (
+              <div>
+                <p>Edit Bio:</p>
+                <textarea value={newBio} onChange={(e) => setNewBio(e.target.value)} />
+                <button onClick={handleEditBio}>Save</button>
+              </div>
+            ) : (
+              <div>
+                <p>{userProfile.bio || "edit bio"}</p>
+              </div>
+            )}
           </div>
-        )}
+        </div>
+        <div className="button">
+          {!editingBio && (
+            <>
+              <button onClick={() => setEditingBio(true)}>Edit Bio</button>
+              <button onClick={() => setEditMode(true)}>Edit profileIMG</button>
+            </>
+          )}
+        </div>
       </div>
-
-      <div>
-        <h2>Posts ({totalPosts}):</h2>
-        {userPosts.map((post) => (
-          <div key={post.id} className="user-post">
-            <p>{post.title} - Likes: {post.likes || 0}</p>
-          </div>
-        ))}
-      </div>
-
-      <p>Total Likes Received: {likesReceived}</p>
     </div>
   );
 }
